ca093a737dc8c9a47293c9741481c1c9d3d50c0c
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-09 22:07:08 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,11 @@
-def using_while
-	levitation_force = 6		 levitation_force = 6
+def using_until
+  levitation_force = 6
+  
+  until levitation_force == 10 
+  puts "Wingardium Leviosa" 
+  counter += 1 
+end
+end 
 
-    while levitation_force < 10 
-	#your code here	      puts "Wingardium Leviosa"
-      levitation_force += 1
-   end
-end	end
+using_until
 
